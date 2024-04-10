c56a48abd2ad316ba33ea0c90fed1d0fbd34b380
IvanvIvanv <148952411+IvanvIvanv@users.noreply.github.com>
2024-04-10 21:21:14 +0300

    Settings in development 2: Saving

--- Assets/Scripts/Global/SettingsSaver.cs
+++ Assets/Scripts/Global/SettingsSaver.cs
@@ -2,23 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using JsonSaverLib;
 
 public class SettingsSaver : MonoBehaviour
 {
     [SerializeField] private string _path;
-    [SerializeField] private Settings _settings;
+    [SerializeField] private Slider[] _sliders;
+
+    private Settings _settings;
 
     [System.Serializable]
     private class Settings
     {
-        public Slider[] _sliders;
+        public FloatSetting FloatSettings;
+    }
+
+    [System.Serializable]
+    private class FloatSetting
+    {
+        public string Name;
+        public float Value;
     }
 
     private void Save()
     {
-        foreach(Slider slider in _settings._sliders)
-        {
-            
-        }
+        Saver.Save(_path, _settings);
+    }
+
+    private void Load()
+    {
+        _settings = Saver.Load<Settings>(_path);
     }
 }
