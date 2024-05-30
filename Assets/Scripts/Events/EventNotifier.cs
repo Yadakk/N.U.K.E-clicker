6d303329f5feb87707882cfc3ccb8695937d214a
Yad <lolyer47@gmail.com>
2024-05-30 12:24:50 +0300

    Added a tooltip for event button

--- Assets/Scripts/Events/EventNotifier.cs
+++ Assets/Scripts/Events/EventNotifier.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class EventNotifier : MonoBehaviour
+public class EventNotifier : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Transform EventFolder;
     public GameObject ExclamationMark;
     [SerializeField] private SoundPlayer _soundPlayer;
     private bool _hasEvents;
+    private bool _isPointerInside;
+    private int _displayedChildCount;
 
     public void Notify()
     {
         if (_soundPlayer != null) _soundPlayer.PlaySound();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _isPointerInside = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isPointerInside = false;
+        Tooltip.HideTooltipStatic();
     }
 
     private void Update()
@@ -21,5 +35,14 @@ public class EventNotifier : MonoBehaviour
         if (_hasEvents == EventFolder.childCount > 0) return;
         _hasEvents = EventFolder.childCount > 0;
         ExclamationMark.SetActive(_hasEvents);
+        UpdateTooltip();
+    }
+
+    private void UpdateTooltip()
+    {
+        if (!_isPointerInside) return;
+        if (_displayedChildCount != EventFolder.childCount)
+            _displayedChildCount = EventFolder.childCount;
+        Tooltip.ShowTooltipStatic(_hasEvents ? "You have " + _displayedChildCount + " unanswered events" : "You have no unanswered events");
     }
 }
